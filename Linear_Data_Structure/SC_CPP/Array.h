f2a378c1a4483b81e3f86694f259e4f1c17fd43b
unknown <2132294@tongji.edu.cn>
2023-04-27 20:09:20 +0800

    Array recorrect

--- Linear_Data_Structure/SC_CPP/Array.h
+++ Linear_Data_Structure/SC_CPP/Array.h
@@ -1,18 +1,54 @@
 #ifndef _ARRAY_H_
 #define _ARRAY_H_
 #include <iostream>
-
-template <typename T>
+#include "LinkList.h"
+#include <optional>
+#include <cstdlib>
+#include <stdexcept>
+template <typename T, int N>
 class Array{
     private:
-        class LinkList{
-            private:
-                T Data;
-                LinkList* Next;
-            public:
-                
-        };
-    
+        T items[N];
+        int num;
+        int qsize;
+    public:
+        Array();
+        Array(T a[], int n);
+        ~Array();
+        T operator[](int n)const;
+        T& operator[](int n);
 };
-
+template<typename T, int N>
+Array<T,N>::Array():num(0),qsize(N)
+{
+}
+template<typename T, int N>
+Array<T,N>::Array(T a[], int n):num(n),qsize(N)
+{
+    for(int i=0;i<n;++i) {
+        items[i] = a[i];
+    }
+}
+template<typename T, int N>
+Array<T,N>:: ~Array()
+{
+}
+template<typename T, int N>
+T Array<T, N>::operator[](int n)const
+{
+    if(n < 0 || n > qsize){
+        throw std::logic_error("ERROR INDEX");
+    }else{
+        return items[n];
+    }
+}
+template<typename T, int N>
+T& Array<T,N>::operator[](int n)
+{
+    if(n < 0 || n > qsize){
+        throw std::logic_error("ERROR INDEX");
+    }else{
+        return items[n];
+    }
+}
 #endif
---
+++ Linear_Data_Structure/SC_CPP/LinkList.h
@@ -0,0 +1,61 @@
+#ifndef _LINKLIST_H_
+#define _LINKLIST_H_
+#include <iostream>
+
+template<typename T>
+class Linklist{
+    private:
+        struct Node{ T Item; Node* Next;};
+        Node* head;
+        int num;
+    public:
+        Linklist();
+        bool isempty()const;
+        bool add(T& item);
+        bool del(T& item);
+        ~Linklist();
+};
+
+template<typename T>
+Linklist<T>::Linklist()
+{
+    head = nullptr;
+    num = 0;
+}
+template<typename T>
+bool Linklist<T>:: isempty()const
+{
+    return (num == 0)? true:false;
+}
+template<typename T>
+bool Linklist<T>:: add(T& item)
+{
+    Node* NewNode = new Node;
+    NewNode->Item = item;
+    NewNode->Next = head->Next;
+    head->Next = NewNode;
+    return true; 
+}
+template<typename T>
+bool Linklist<T>:: del(T& item)
+{
+    if(isempty()) return false;
+    else{
+        Node* temp = head;
+        item = head->Item;
+        head = head->Next;
+        delete temp;
+        return true;
+    }
+}
+template<typename T>
+Linklist<T>:: ~Linklist()
+{
+    Node* temp = head;
+    while(head){
+        head = head->Next;
+        delete temp;
+        temp = head;
+    }
+}
+#endif
